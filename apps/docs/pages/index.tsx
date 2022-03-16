2e23711afcf78df50317d324b0f20bc39f231e22
ppuliyurudaiyan <ppuliyurudaiyan@paypal.com>
2022-03-16 16:15:47 -0700

    title

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -11,12 +11,12 @@ export default function Docs() {
     <div className="bg-background">
       <Head>
         <title>MamuLang - A toy tamil programming language based on an inside joke</title>
-        <meta property="og:title" content="Bhailang - A toy programming language based on an inside joke" key="title" />
+        <meta property="og:title" content="MamuLang -  A toy tamil programming language based on an inside joke" key="title" />
         <meta property="og:type" content="website" key="type" />
         <meta property="og:url" content="https://bhailang.js.org" key="url" />
-        <meta property="og:description" content="Bhailang is dynamically typed toy programming language, based on an inside joke, written in Typescript. Created by Aniket Singh and Rishabh Tripathi." key="description" />
-        <meta name="description" content="Bhailang is dynamically typed toy programming language, based on an inside joke, written in Typescript. Created by Aniket Singh and Rishabh Tripathi." />
-        <meta property="og:site_name" content="Bhailang Documentation" key="siteName" />
+        <meta property="og:description" content="MamuLang is dynamically typed toy programming language, based on an inside joke, written in Typescript. Created by Aniket Singh and Rishabh Tripathi." key="description" />
+        <meta name="description" content="MamuLang is dynamically typed toy programming language, based on an inside joke, written in Typescript. Created by Aniket Singh and Rishabh Tripathi." />
+        <meta property="og:site_name" content="MamuLang Documentation" key="siteName" />
       </Head>
       <Header />
       <Code />
