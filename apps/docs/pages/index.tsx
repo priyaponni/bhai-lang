5d21c20084e66f40bb43fcfb5e4f4c7234bea513
ppuliyurudaiyan <ppuliyurudaiyan@paypal.com>
2022-03-16 16:10:00 -0700

    title

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Docs() {
   return (
     <div className="bg-background">
       <Head>
-        <title>Bhailang - A toy programming language based on an inside joke</title>
+        <title>MamuLang - A toy tamil programming language based on an inside joke</title>
         <meta property="og:title" content="Bhailang - A toy programming language based on an inside joke" key="title" />
         <meta property="og:type" content="website" key="type" />
         <meta property="og:url" content="https://bhailang.js.org" key="url" />
